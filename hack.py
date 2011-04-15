1e06ddf6010177453840689a895441d64f0827b6
Chaz Lever <register@chazlever.com>
2011-04-15 13:40:28 -0400

    Modify submission script to prompt for passphrase.

--- hack.py
+++ hack.py
@@ -1,13 +1,17 @@
+#!/usr/bin/env python
+
 import urllib, hashlib 
 
 # Get input from user
-passphrase = hashlib.md5("I ate the dingo's baby.").hexdigest()
+passphrase = raw_input("Enter the passphrase: ")
+passphrase = hashlib.md5(passphrase).hexdigest()
 address = raw_input("Enter your IP address: ")
 hack = raw_input("Enter your hack: ")
 description = raw_input("Enter hack description: ")
 
 # Build and send request to server
-mParams = 'passphrase=' + passphrase + ':address=' + address + ':hack=' + hack + ':description=' + description
+mParams = 'passphrase=' + passphrase + ':address=' + address + \
+          ':hack=' + hack + ':description=' + description
 params = urllib.urlencode({ 'method' : 'recordHack', 'params' :  mParams })
 f = urllib.urlopen("http://localhost/~chaz/hackathon/api.php", params)
 result = f.read()
